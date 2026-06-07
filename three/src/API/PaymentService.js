0130ee5585ab020408a602729986d9fe059fa2da
AntonShuliak <antonshulyak0@gmail.com>
2026-06-07 17:29:41 +0300

    Fix payment URLs for production

--- three/src/API/PaymentService.js
+++ three/src/API/PaymentService.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
 export default class PaymentService {
   static async createCheckoutSession() {
     return axios.post(
-      "http://127.0.0.1:8001/create-checkout-session",
+      "https://web-music-system-production.up.railway.app/create-checkout-session",
       {},
       {
         headers: {
