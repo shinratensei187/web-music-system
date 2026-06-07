8ec2bb7148bcc5526751ceb3dc30bb16579855b4
AntonShuliak <antonshulyak0@gmail.com>
2026-06-07 13:45:29 +0300

    Point frontend API to Railway backend

--- three/src/API/api.js
+++ three/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8001",
+  baseURL: "https://web-music-system-production.up.railway.app/",
 });
 
 API.interceptors.request.use((config) => {
